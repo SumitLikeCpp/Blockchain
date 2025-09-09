31a951e0b2ac8de7b483c494bf9509dbce481074
SumitLikeCpp <sum8tkumar39699@gmail.com>
2025-09-10 05:29:13 +0530

    constructor

---
+++ constructor.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract ExampleConstructor{
+    address public myAddress;
+
+    function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+
+    function setMyAddressToMsgSender() public{
+        myAddress = msg.sender;
+    }
+}
