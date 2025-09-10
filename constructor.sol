2c311643df2f6e251d9890e4975f0fc288313831
SumitLikeCpp <sum8tkumar39699@gmail.com>
2025-09-10 05:38:44 +0530

    constructor done

--- constructor.sol
+++ constructor.sol
@@ -5,6 +5,13 @@ pragma solidity 0.8.24;
 contract ExampleConstructor{
     address public myAddress;
 
+    // a constructor is a special function that is executed only once when a contract is deployed. 
+    // It is used to initialize state variables or perform setup tasks necessary for the contract’s operation.
+
+    constructor(address _someAddress){
+        myAddress = _someAddress;
+    }
+
     function setMyAddress(address _myAddress) public {
         myAddress = _myAddress;
     }
